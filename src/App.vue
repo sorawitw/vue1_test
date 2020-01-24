7c59ffe74a083f10ba30561f9e730268dc4ba892
rispro <rispro.ray@gmail.com>
2020-01-24 14:24:06 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <b-navbar toggleable="lg" type="dark" variant="dark">
+        <b-navbar-brand href="#/">Vue</b-navbar-brand>
+        <b-navbar-toggle target="nav_collapse"/>
+        <b-collapse is-nav id="nav_collapse">
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#/">Home</b-nav-item>
+            <b-nav-item href="#/about">About</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
-    <router-view />
+    <b-container>
+      <router-view/>
+    </b-container>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,73 @@
 <template>
   <div class="home">
+    <div id="list-complete-demo" class="demo">
+      <b-button variant="primary" v-on:click="add">Add</b-button>
+      <b-button variant="danger" v-on:click="remove">Remove</b-button>
+      <transition-group name="list-complete" tag="p">
+        <span v-for="item in items" v-bind:key="item" class="list-complete-item">{{ item.n }}</span>
+      </transition-group>
+      <b-form-input v-model="filter" placeholder="Type to Search" />
+      <div>
+        <b-table striped :items="items" :filter="filter" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data: function() {
+    return {
+      items: [
+        { "#": 1, n: "abc" },
+        { "#": 2, n: "def" }
+      ],
+      nextNum: 3,
+      filter: null
+    };
+  },
+  methods: {
+    randomIndex: function() {
+      return Math.floor(Math.random() * this.items.length);
+    },
+    randomAphabet: function() {
+      var chars = "abcdefghijklmnopqrstuvwxyz";
+      return chars.charAt(Math.floor(Math.random() * chars.length));
+    },
+    add: function() {
+      this.items.splice(this.randomIndex(), 0, {
+        "#": this.nextNum++,
+        n: this.randomAphabet() + this.randomAphabet() + this.randomAphabet()
+      });
+    },
+    remove: function() {
+      this.items.splice(this.randomIndex(), 1);
+    }
+  }
+};
+</script>
+
+<style>
+.list-complete-item {
+  transition: all 1s;
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-complete-enter,
+.list-complete-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+.list-complete-leave-active {
+  position: absolute;
+}
+</style>
+
+
+<!-- 
+<template>
+  <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -16,3 +84,4 @@ export default {
   }
 };
 </script>
+-->
